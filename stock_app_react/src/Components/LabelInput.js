6612bd4d836fb43422e04f0d9cdcb0b08b170682
Stanley Tarce <stanleytarce18@gmail.com>
2021-12-22 13:42:36 +0800

    Refactored Input Label Type Component

--- stock_app_react/src/Components/LabelInput.js
+++ stock_app_react/src/Components/LabelInput.js
@@ -1,11 +1,9 @@
-import React, { useEffect, useRef } from 'react'
-
-function LabelInput(svg = null, state, setState, type) {
-  const ref = useRef()
+import React, { useEffect, forwardRef } from 'react'
+const LabelInput = forwardRef(({ svg = null, state, setState, type, children = null }, ref) => { // Pass ref to this component
   const activeStatus = state[type] ? "translate-y-[-13px]" : "translate-y-0"
   const clickOutside = (state, ref, setState, type) => {
     return e => {
-      if (state && !ref.current.contains(e.target) && ref.current && ref.current.value.length === 0) {
+      if (state[type] && !ref.current.contains(e.target) && ref.current && ref.current.value.length === 0) {
         setState({ ...state, [type]: false })
       }
     }
@@ -20,7 +18,7 @@ function LabelInput(svg = null, state, setState, type) {
     , [state[type]]
   )
   return (
-    type === 'password' ? (<div className='bg-[#ECF0F1] w-full h-[50px] flex justify-start items-center gap-[5px] font-bold text-primary-gray'>
+    <div className='bg-[#ECF0F1] w-full h-[50px] flex justify-start items-center gap-[5px] font-bold text-primary-gray'>
       <div className="w-[10%] flex justify-center items-center">
         {svg}
       </div>
@@ -28,18 +26,10 @@ function LabelInput(svg = null, state, setState, type) {
         <label className={`absolute ${activeStatus} transition pointer-events-none`}>{type.toUpperCase()}</label>
         <input ref={ref} onClick={() => setState({ ...state, [type]: true })} className='text-black bg-transparent w-full relative outline-none' type={type} />
       </div>
-      <p className='text-[12px] mr-1 text-primary-blue-light'>FORGOT</p>
-    </div>) : (<div className='bg-[#ECF0F1] w-full h-[50px] flex justify-start items-center gap-[5px] font-bold text-primary-gray'>
-      <div className="w-[10%] flex justify-center items-center">
-        {svg}
-      </div>
-      <div className="flex flex-col align-center justify-center h-full text-[13px] overflow-hidden w-[90%]">
-        <label className={`absolute ${activeStatus} transition pointer-events-none`}>{type.toUpperCase()}</label>
-        <input ref={ref} onClick={() => setState({ ...state, [type]: true })} className='text-black bg-transparent w-full relative outline-none' type={type} />
-      </div>
-    </div>)
-
+      {children}
+    </div>
   )
-}
+})
+
 
 export default LabelInput
