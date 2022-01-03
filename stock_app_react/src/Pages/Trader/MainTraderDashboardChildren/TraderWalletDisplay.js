943b6e7372b63f86019ebb4175cda9ab86a2158a
Miyu Togo <togomiyu@gmail.com>
2022-01-03 20:56:01 +0800

    trader user dashboard complete

--- stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderWalletDisplay.js
+++ stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderWalletDisplay.js
@@ -15,20 +15,20 @@ return(
                     </div>
                     <div  className='p-3.5'>
                         <WalletCard/>
-                        <h1>Hi</h1>
                     </div>
                 </div>
 
                 {/* Deposit and Withdraw */}
                 <div className='w-auto h-[60px] bg-container-light-blue rounded-[20px] flex justify-start items-center gap-2 p-2'>
-                      
+                      <h2>Deposit</h2>
                 </div>
 
                 {/* Assets and Transactions */}
                 <div>
                      {/* Header */}
                     <div className='w-auto h-[60px] flex justify-start items-center gap-2 p-2'>
-                        <h2 className=' text-white text-[20px]'> </h2>
+                        <h2 className=' text-white text-[20px]'> Assets </h2>
+                        <h2 className=' text-white text-[20px]'> Transactions </h2>
                     </div>
 
                     {/* Market */}
@@ -38,10 +38,10 @@ return(
                 </div>
 
                 {/* Details Tabs */}
-                <div>
-                    <p>Stock</p>
-                    <p>Unit</p>
-                    <p>Price</p>
+                <div className='w-full pr-10 flex flex-row justify-end items-center gap-[30px]'>
+                    <p className='text-white'>Stock</p>
+                    <p className='text-white'>Unit</p>
+                    <p className='text-white'>Price</p>
                 </div>
 
                 {/* Asset */}
