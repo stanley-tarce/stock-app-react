25ab1d9afc6c3614ec8ccd022d2975fb398fc6a9
Leandra Jade Panopio <leandrapanopio@gmail.com>
2022-02-08 05:09:25 +0800

    fix markets display; responsive sizes

--- stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderHomeDisplay.js
+++ stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderHomeDisplay.js
@@ -11,7 +11,7 @@ function TraderHomeDisplay() {
     return (
         <div className='w-full h-full flex flex-col justify-center items-center'>
             {/* Dashboard header */}
-            <p className='w-[90%] h-fit mt-5 text-white text-xl font-primary lg:mt-20 py-[1%]  xl:text-2xl'>Dashboard</p>
+            <p className='w-[90%] h-fit mt-5 text-white text-xl font-primary lg:mt-20 py-[1%]  md:text-2xl'>Dashboard</p>
 
             <div className='h-[30%] w-[90%] md:bg-primary-blue rounded-lg flex mt-4 justify-center'>
                 <div className='for-padding md:flex justify-center items-center md:py-4 lg:py-6'>
@@ -38,7 +38,7 @@ function TraderHomeDisplay() {
                         }
                         return <div key={index} className='w-full min-h-[15%] lg:min-h-[25%] flex text-base xl:text-2xl md:text-xl justify-between items-center bg-container-light-blue rounded-lg hover:bg-blue-400 p-2 cursor-pointer' onClick={goToMarket}>
                             <span className='w-[50%] flex pointer-events-none items-center justify-start text-left'>
-                                <img className="rounded-[20px] pointer-events-none" width="45" height="45" src={market.logo.split('"')[1]} />
+                                <img className="rounded-full object-cover pointer-events-none" width="40" height="40" src={market.logo.split('"')[1]} />
                                 <p className=' text-left pl-4 pointer-events-none'>{market.symbol}</p>
                             </span>
                             <p className='mr-4 pointer-events-none'>${market.price_per_unit}</p>
@@ -46,9 +46,6 @@ function TraderHomeDisplay() {
                     }) : <p className='w-[90%] h-auto text-white text-[20px]'>No Market Found</p>}
                 </div>
             </div>
-
-
-
         </div>
     )
 }
--- stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderMarketDisplay.js
+++ stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderMarketDisplay.js
@@ -17,22 +17,32 @@ function TraderMarketDisplay() {
 
     return navigate(`${id}`)
   }
+ 
   return (
     <section className='w-full h-full flex flex-col items-center text-white'>
-      <h2 className='w-[90%] h-auto text-xl md:text-2xl  mt-5 border-white mb-[15px]'>Markets</h2>
+      <h2 className='w-[90%] h-auto text-xl md:text-2xl mt-5 border-white mb-[15px]'>Markets</h2>
       <div className='w-[90%] h-[83%] flex flex-col gap-4'>
-        <div className='w-full h-[60px] bg-container-light-blue rounded-[20px] flex justify-start items-center gap-2 p-2'>
-          <SearchIcon />
-          <input className='w-[90%] h-[95%] bg-transparent outline-none text-white text-[20px]' placeholder='Search Market Here' onChange={(event) => setSearch(event.target.value)} />
+        <div className='w-full h-[50px] border-2 border-container-light-blue rounded-lg flex justify-start items-center gap-2 md:py-6 p-2'>
+          <SearchIcon className={"w-[30px] h-[30px] text-text-gray"} />
+          <input className='w-[90%] h-[95%] bg-transparent md:py-4 outline-none text-white text-base md:text-lg' placeholder='Search Market Here' onChange={(event) => setSearch(event.target.value)} />
         </div>
-        <div className='w-full h-full overflow-scroll flex flex-col gap-4'>
+        <div className='table-head w-full h-auto grid grid-cols-4'>
+          <p className='w-full h-full flex col-start-1 col-end-3 justify-start text-xs md:text-base ml-4 md:ml-8 uppercase text-white'>stock</p>
+          <p className='w-full h-full flex col-start-3 col-end-4 justify-start text-xs md:text-base uppercase text-white'>price</p>
+          <p className='w-full h-full flex col-start-4 col-end-5 justify-start text-xs md:text-base uppercase text-white'>chg%</p>
+        </div>
+        <div className='w-full h-full overflow-scroll flex flex-col gap-4 '>
           {markets.length !== 0 ? markets.filter(data => data.symbol.includes(search)).map((market, index) => {
-            return <div key={index} onClick={(e) => goToMarket(e)} className='w-full flex text-[20px] justify-between items-center bg-container-light-blue rounded-[20px] hover:bg-blue-400 p-2 cursor-pointer'>
-              <span className='w-[50%] flex pointer-events-none items-center justify-start text-left'>
-                <img className="rounded-[20px] pointer-events-none" width="45" height="45" src={market.logo.split('"')[1]} />
-                <p className=' text-left pl-4 pointer-events-none'>{market.symbol}</p>
+            return <div key={index} onClick={(e) => goToMarket(e)} className='w-full grid grid-cols-4 min-h-[55px] lg:min-h-[60px] flex text-base xl:text-2xl md:text-xl justify-center items-center bg-container-light-blue rounded-lg hover:bg-blue-400 p-2 cursor-pointer'>
+              <span className='w-[50%] flex md:pl-6 pointer-events-none col-start-1 col-end-3 items-center justify-start text-left'>
+                <img className="rounded-full pointer-events-none" width="40" height="40" src={market.logo.split('"')[1]} />
+                <p className='text-base pl-4 xl:text-2xl md:text-xl pointer-events-none'>{market.symbol}</p>
               </span>
-              <p className='mr-4 pointer-events-none'>${market.price_per_unit}</p>
+              <p className='mr-4 col-start-3 col-end-4 flex justify-start pointer-events-none'>${market.price_per_unit}</p> 
+              {market.percentage_change && market.percentage_change.includes("+")   
+                ? <p className='mr-4 col-start-4 text-primary-green col-end-5 flex justify-end md:justify-start pointer-events-none'>{market.percentage_change}</p>
+                : <p className='mr-4 col-start-4 text-primary-red col-end-5 flex justify-end md:justify-start pointer-events-none'>{market.percentage_change}</p>
+              }            
             </div>
           }) : <p className='w-[90%] h-auto text-white text-[20px]'>No Market Found</p>}
         </div>
