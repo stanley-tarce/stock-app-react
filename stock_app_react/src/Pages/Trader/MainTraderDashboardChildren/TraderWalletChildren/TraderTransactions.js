3bdbb0e6c97cb8b8869c198f3f139008aeb8af0f
Leandra Jade Panopio <leandrapanopio@gmail.com>
2022-02-10 21:34:53 +0800

    fixing wallet display - add responsive design to wallet

--- stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderWalletChildren/TraderTransactions.js
+++ stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderWalletChildren/TraderTransactions.js
@@ -5,18 +5,18 @@ function TraderTransactions() {
     const { totalData } = useContext(CreateContext)
     const { TRADERTRANSACTIONS } = totalData
     return (
-        <div className='w-auto h-auto flex flex-col justify-start items-center  '>
+        <div className='w-auto h-full flex flex-col justify-start items-center  '>
             <div className='w-full h-auto flex flex-col justify-start items-center gap-3'>
                 {TRADERTRANSACTIONS && TRADERTRANSACTIONS?.map((transaction, index) => {
                     const { symbol, transaction_type, total_price } = transaction
                     // var newLogo = logo.split('"')[1] 
-                    return <div key={index} className="w-full h-[50px] rounded-[20px] bg-primary-button-blue-light py-3 flex justify-center items-center">
-                        <div className='w-1/3 gap-3 h-auto flex justify-center items-center'>
+                    return <div key={index} className="w-full min-h-[55px] lg:min-h-[60px] grid grid-cols-3 rounded-lg bg-primary-button-blue-light py-3 flex justify-center items-center">
+                        <div className='w-full col-start-1 col-end-2 gap-3 h-auto flex justify-center items-center'>
                             {/* <img src={newLogo} alt='logo' className='w-[30px] h-[30px] rounded-full' /> */}
-                            <p className='text-[20px] text-white'>{symbol}</p>
+                            <p className='text-base text-white'>{symbol}</p>
                         </div>
-                        <div className='w-1/3 text-gray-300 text-[20px] flex justify-center items-center'>{Math.floor(total_price)}</div>
-                        <div className='w-1/3 text-gray-300 text-[20px] flex justify-center items-center'>{transaction_type}</div>
+                        <div className='w-full col-start-2 col-end-3 text-gray-300 text-base flex justify-center items-center'>${Math.floor(total_price)}</div>
+                        <div className='w-full col-start-3 col-end-4 text-gray-300 text-base flex justify-center uppercase items-center'>{transaction_type}</div>
                     </div>
                 })}
             </div>
--- stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderWalletDisplay.js
+++ stock_app_react/src/Pages/Trader/MainTraderDashboardChildren/TraderWalletDisplay.js
@@ -19,62 +19,56 @@ function TraderWalletDisplay() {
     transactions: () => navigate('transactions')
   }
   return (
-    <div className='w-screen h-screen bg-primary-blue-light flex flex-col items-center gap-[15px]'>
+    <div className='w-full h-screen flex flex-col items-center justify-center gap-[15px]'>
       <p className='w-[90%] text-white h-auto text-xl md:text-2xl mt-5 border-white font-primary'>Wallet</p>
-      <div className='balance-walletCard relative w-[300px]'>
-        <WalletCard className="w-full h-[200px] flex z-0" /> 
-        <div className='w-full h-auto absolute z-[1] flex flex-col justify-start items-start top-[50px]'>
-          <div className='w-full h-auto text-base flex justify-between items-center px-5 text-white'>
-            <p className='text-white font-thin'>Current Balance</p>
-            <p className='date'>{stringDate}</p>
+      <div className='desktop-view w-[90%] h-[90%] lg:flex items-center justify-around lg:relative'>
+        <div className='balance-walletCard flex justify-center relative w-[320px]'>
+          <WalletCard className="w-full h-[200px] flex z-0" /> 
+          <div className='w-full h-auto absolute z-[1] flex flex-col justify-start items-start top-[50px]'>
+            <div className='w-full h-auto text-base flex justify-between items-center px-5 text-white'>
+              <p className='text-white font-thin font-primary'>Current Balance</p>
+              <p className='date'>{stringDate}</p>
+            </div>
+            <p className='balance w-full h-auto text-3xl mx-5 my-2 font-bold text-white'>{`$${totalData.TRADERINFO.wallet ? totalData.TRADERINFO.wallet.toFixed(1) : 0}`}</p>
           </div>
-          <p className='balance w-full h-auto text-3xl mx-5 my-2  text-white font-bold'>{`$${totalData.TRADERINFO.wallet ? totalData.TRADERINFO.wallet.toFixed(1) : 0}`}</p>
         </div>
-      </div>
-      {/* <div className='w-[90%] h-auto absolute z-[1] flex flex-col justify-start items-center bottom-[350px]'>
-        <div className='w-full h-auto text-[20px] flex justify-between items-center px-5 text-white'>
-          <p className='text-white font-thin'>Current Balance</p>
-          <p>{stringDate}</p>
-        </div>
-        <p className='w-full h-auto text-[35px] px-5 text-white font-bold'>{`$${totalData.TRADERINFO.wallet ? totalData.TRADERINFO.wallet.toFixed(1) : 0}`}</p>
-      </div> */}
 
-      <div className='w-[90%] h-auto grid grid-cols-2 items-center px-5 text-white relative z-10 '>
-        <span className='w-full flex justify-start col-start-1 col-end-2'>
-          <button className='w-fit h-fit flex justify-center py-3 px-8 rounded-lg bg-primary-green cursor-pointer' onClick={() => navigate('../deposit')}>Deposit</button>
-        </span>
-        <span className='w-full flex justify-start col-start-2 col-end-3'>
-          <button className='w-fit h-fit flex justify-center  mx-2 py-3 px-8 rounded-lg bg-container-light-blue cursor-pointer' onClick={() => navigate('../withdraw')}>Withdraw</button>          
-        </span>
+        <div className='deposit-withdraw-btns w-full mt-4 lg:w-[50%] lg:h-full h-auto flex justify-center text-white'>
+          <div className='h-full w-full flex lg:flex-col items-center justify-around'>
+            <span className='w-full flex justify-start'>
+              <button className='min-w-fit h-fit lg:w-full flex justify-center py-3 px-8 rounded-lg bg-primary-green cursor-pointer font-primary' onClick={() => navigate('../deposit')}>Deposit</button>
+            </span>
+            <span className='w-full flex justify-end'>
+              <button className='min-w-fit h-min-fit lg:w-full py-3 px-8 rounded-lg bg-container-light-blue cursor-pointer font-primary' onClick={() => navigate('../withdraw')}>Withdraw</button>          
+            </span>
+          </div>    
+        </div>
       </div>
+     
 
       <div className='mt-2 w-[90%]'>
-        <div className='w-full h-full text-base flex justify-center grid grid-cols-2 rounded-lg'>
-          {/* <button onClick={handleButton.assets} ref={assetsRef} 
-            className={`rounded-lg bg-primary-navbar-color-blue h-fit col-start-1 col-end-2 py-3 px-8 relative left-4 hover:bg-container-light-blue cursorpointer text-white hover:z-10 
-            ${location.pathname === '/main/wallet' ? 'bg-container-light-blue z-10' : 'bg-primary-navbar-color-blue'}`}>Assets
-          </button> */}
+        <div className='max-w-full h-full text-base flex justify-center grid grid-cols-2 rounded-lg'>
           <button onClick={handleButton.assets} ref={assetsRef} 
-            className={`rounded-lg bg-primary-navbar-color-blue h-fit col-start-1 col-end-2 py-3 px-8 relative left-4 hover:bg-container-light-blue cursorpointer text-white hover:z-10 
+            className={`rounded-lg bg-primary-navbar-color-blue h-fit col-start-1 col-end-2 py-3 px-8 relative left-4 hover:bg-container-light-blue cursorpointer text-white font-primary hover:z-10 
             ${location.pathname === '/main/wallet' ? 'bg-container-light-blue z-10' : 'bg-primary-navbar-color-blue'}`}>Assets
           </button>
           
           <button onClick={handleButton.transactions} ref={transactionRef} 
-            className={` bg-primary-navbar-color-blue rounded-lg h-fit col-start-2 col-end-3 py-3 px-8 relative right-4 hover:bg-container-light-blue cursorpointer text-white hover:z-10 
+            className={` bg-primary-navbar-color-blue rounded-lg h-fit col-start-2 col-end-3 py-3 px-8 relative right-4 hover:bg-container-light-blue cursorpointer text-white font-primary hover:z-10 
             ${location.pathname === '/main/wallet/transactions' ? ' bg-container-light-blue z-10' : ' bg-primary-navbar-color-blue'} `} >Transactions
           </button>
         </div>
       </div>
-      {location.pathname === '/main/wallet' ? <div className='w-[90%] h-auto flex justify-center items-center'>
-        <p className='w-1/3 h-auto flex items-center justify-center text-xs md:text-base ml-4 md:ml-8 uppercase text-white'>stock</p>
-        <p className='w-1/3 h-auto flex items-center justify-center text-xs md:text-base ml-4 md:ml-8 uppercase text-white'>price</p>
-        <p className='w-1/3 h-auto flex items-center justify-center text-xs md:text-base ml-4 md:ml-8 uppercase text-white'>unit</p>
-      </div> : <div className='w-[90%] h-auto flex justify-center items-center'>
-        <p className='w-1/3 h-auto flex items-center  justify-center text-[15px] text-white'>STOCK</p>
-        <p className='w-1/3 h-auto flex items-center  justify-center text-[15px] text-white'>PRICE</p>
-        <p className='w-1/3 h-auto flex items-center  justify-center text-[15px] text-white'>TYPE</p>
+      {location.pathname === '/main/wallet' ? <div className='w-auto h-auto flex justify-center items-center'>
+        <p className='w-full h-auto flex items-center justify-center text-xs md:text-base ml-4 md:ml-8 uppercase text-white'>stock</p>
+        <p className='w-full h-auto flex items-center justify-center text-xs md:text-base ml-4 md:ml-8 uppercase text-white'>price</p>
+        <p className='w-full h-auto flex items-center justify-center text-xs md:text-base ml-4 md:ml-8 uppercase text-white'>unit</p>
+      </div> : <div className='w-[90%] mr-6 rounded-lg grid grid-cols-3 h-auto '>
+        <p className='w-full h-auto flex col-start-1 col-end-2 items-center text-xs md:text-base ml-4 md:ml-8 uppercase  justify-center text-white'>stock</p>
+        <p className='w-full h-auto flex col-center-2 col-end-3 items-center text-xs md:text-base ml-4 md:ml-8 uppercase justify-center text-white'>price</p>
+        <p className='w-full h-auto flex col-center-3 col-end-4 items-center text-xs md:text-base ml-4 md:ml-8 uppercase justify-center text-white'>type</p>
       </div>}
-      <div className='w-[90%] h-[150px] max-h-[130px] pb-30 overflow-y-scroll '>
+      <div className='w-[90%] h-full overflow-y-scroll '>
         <Outlet />
       </div>
     </div>
